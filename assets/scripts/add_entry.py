9fab685d7c24ea7bd08128b02e57fb6ba08e31e8
Neel Patel <neelpatel1337@gmail.com>
2023-05-22 23:43:56 -0400

    Adjusted add_entry to more flexible, added more albums

--- assets/scripts/add_entry.py
+++ assets/scripts/add_entry.py
@@ -2,29 +2,41 @@ from auth import get_token
 from album import search_album, get_album_data
 import json
 from os import path
-import random
+import sys
 
-ALBUM_TEXT_FILE = '../not_working.txt'
 JSON_FILE = '../albums.json'
+
 
 def add_to_json(album_data):
     with open(JSON_FILE, "r") as read_file:
         data = json.load(read_file)
+
+    for entry in data:
+        if album_data['name'] == entry['name'] and album_data['artists'] == entry['artists']:
+            print('Album already in JSON file, aborting.')
     data.append(album_data)
     
     with open(JSON_FILE, "w") as write_file:
         json.dump(data, write_file)
+    print('Success!')
 
 token = get_token()
 
-albums = []
-with open(ALBUM_TEXT_FILE, 'r') as file:
-    for a in file:
-        albums.append(a.strip())
+if __name__ == "__main__":
+    album = str(sys.argv[1])
+    if str(sys.argv[1]) == '--manual':
+        print(sys.argv[0])
+    album_token = search_album(album, token)
 
-for a in albums:
-    print('indexing ' + a)
-    album_token = search_album(a, token)
-    if album_token is not None:
-        album_data = get_album_data(album_token, token)
+    if album_token is None:
+        print('Error: No album id')
+
+    album_data = get_album_data(album_token, token)
+    print('Add', album_data['name'],  'by',album_data['artists'], '? (y/n)')
+    i = input()
+    if i.lower() in ['', 'y', 'yes']:
         add_to_json(album_data)
+    else:
+        print('Aborting')
+
+
